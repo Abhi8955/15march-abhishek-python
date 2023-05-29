cd052a3a4cf6ef67e1172927290615bf229847a7
Abhi8955 <120082500+Abhi8955@users.noreply.github.com>
2023-05-29 13:07:36 +0530

    Delete Assignment/module-3 directory

--- Assignment/module-3/Duplicate remove.py
+++
@@ -1,9 +0,0 @@
-def remove(s):
-    s=list(s)
-    #print(s)
-    return s
-s={1,2,3,4,4,5,2}
-
-x=remove(s)
-
-print(list(x))
--- Assignment/module-3/Tuple reverse.py
+++
@@ -1,4 +0,0 @@
-mytup=('one','two','three','foue')
-list=mytup
-list.__reversed__()
-print((list))
